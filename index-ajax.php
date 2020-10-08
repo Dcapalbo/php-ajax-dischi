ec57ea073f5486e08cfdbdcf21c7c9ea88b2de9a
Domenico Capalbo <capalbodomenico@gmail.com>
2020-10-08 20:33:21 +0200

    opzional-milestone-complete
    php-ajax-dischi

--- index-ajax.php
+++ index-ajax.php
@@ -7,6 +7,9 @@
      <div class="header_wrapper">
        <!-- logo -->
       <i class="fab fa-spotify"></i>
+      <select class="select_author" name="">
+       <option value="all">Authors</option>
+      </select>
      </div>
     </div>
    </header>
@@ -15,20 +18,26 @@
     <!-- main -->
     <!-- wrapper -->
     <!-- append handlebars template -->
-    <ul class="albums_list_wrapper">
-     </ul>
+    <ul class="albums_list">
+    </ul>
    <!-- end album cover wrapper -->
    </main>
    <!-- end main -->
   </body>
   <!-- handlebars template -->
+  <!-- album template -->
   <script id="album-template" type="text/x-handlebars-template">
-   <li class="album_cards">
+   <li class="album_cards" data-author="{{author}}">
     <img src="{{{poster}}}" alt="#">
     <h3>{{title}}</h3>
     <h4>{{author}}</h4>
     <p>{{year}}</p>
    </li>
+  </script>
+  <!-- end album template -->
+  <!-- author selection template -->
+  <script id="author_template" type="text/x-handlebars-template">
+   <option value="{{author}}">{{author}}</option>
   </script>
   <!-- end handlebars template -->
   <script src="dist/app.js" charset="utf-8"></script>
--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ const $ = require('jquery');
 const Handlebars = require('handlebars');
 
 $(document).ready(function() {
+
   //make an ajax call to GET the key from the API
   $.ajax (
      {
       "url": "http://localhost:8888/php-ajax-dischi/server.php",
       "method": "GET",
       "success": function(albums) {
-        //invoke the printData function
-        printData(albums);
+        //invoke functions to print the inputs from the API
+        printData(albums, "all");
+        printAuthors(albums);
     },
       "error": function (err) {
        alert("There is an error with the Ajax call. " + err);
@@ -17,23 +19,65 @@ $(document).ready(function() {
   });
   // Functions
   //make a function to print the data of the Api
-  function printData(albums) {
+  function printData(albums, filter) {
     //Prepare the handlebars template
     var source = $("#album-template").html();
     var template = Handlebars.compile(source);
     //make a "cicle for" to get inside the Json objects
-    for (var i = 0; i < albums.length; i++) {
-     var context = {
-     //find property which are going to be inserted in the HTML placeholders
-     "title": albums[i].title,
-     "author": albums[i].author,
-     "year": albums[i].year,
-     "poster": albums[i].poster
-  };
+      for (var i = 0; i < albums.length; i++) {
+        var context = {
+        //find property which are going to be inserted in the HTML placeholders
+        "title": albums[i].title,
+        "author": albums[i].author,
+        "year": albums[i].year,
+        "poster": albums[i].poster
+      };
     //make an html variable with inside the handlebars context
     var html = template(context);
     //append the HTML inside the DOM
-    $(".albums_list_wrapper").append(html);
+    if (filter == "all") {
+      $(".albums_list").append(html);
+    } else if (filter == albums[i].author) {
+      $(".albums_list").append(html);
+    }
   }
  }
+  //make a function to print the data of the Api
+  function printAuthors(albums) {
+    //Prepare the handlebars template
+    var source = $("#author_template").html();
+    var template = Handlebars.compile(source);
+    //make a "cicle for" to get inside the Json objects
+     for (var i = 0; i < albums.length; i++) {
+       var context = {
+       //find property which are going to be inserted in the HTML placeholders
+       "author": albums[i].author
+       };
+       //make an html variable with inside the handlebars context
+       var html = template(context);
+       //append the HTML inside the DOM
+       $(".select_author").append(html);
+     }
+   }
+  $(".select_author").change(function() {
+   //make a variable to save the value
+   var authorValue = $(this).val();
+   resetData();
+   $.ajax (
+      {
+       "url": "http://localhost:8888/php-ajax-dischi/server.php",
+       "method": "GET",
+       "success": function(albums) {
+         //invoke functions to print the inputs from the API
+         printData(albums, authorValue);
+     },
+       "error": function (err) {
+        alert("There is an error with the Ajax call. " + err);
+      }
+   });
+  });
+    //make a function to reset the DOM
+    function resetData() {
+      $(".albums_list").html("");
+    }
 });
